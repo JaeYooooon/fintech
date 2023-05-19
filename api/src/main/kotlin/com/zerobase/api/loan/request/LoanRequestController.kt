d4ef5d615e8c18b06c2e06bb442c924577c83cee
JaeYooooon <dhy04050@naver.com>
2023-05-19 13:54:37 +0900

    test code controller 테스트 코드 작성

--- api/src/main/kotlin/com/zerobase/api/loan/request/LoanRequestController.kt
+++ api/src/main/kotlin/com/zerobase/api/loan/request/LoanRequestController.kt
@@ -1,6 +1,5 @@
 package com.zerobase.api.loan.request
 
-
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
