b4c1e5715e1d6fb150185dadbb20c237cf3eec23
JaeYooooon <dhy04050@naver.com>
2023-05-19 11:51:40 +0900

    create swagger config 암호화, 복호화 스웨거 적용

---
+++ api/src/main/kotlin/com/zerobase/api/loan/request/LoanRequestService.kt
@@ -0,0 +1,17 @@
+package com.zerobase.api.loan.request
+
+import com.zerobase.domain.domain.UserInfo
+import org.springframework.stereotype.Service
+
+
+interface LoanRequestService {
+    fun loanRequestMain(
+            loanRequestInputDto: LoanRequestDto.LoanRequestInputDto
+    ): LoanRequestDto.LoanRequestResponseDto
+
+    fun saveUserInfo(
+        userInfoDto: UserInfoDto
+    ): UserInfo
+
+    fun loanRequestReview(userKey: String)
+}
