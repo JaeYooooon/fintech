f30993c95945efd6b5aed0765ad7c7a442c14e95
JaeYooooon <dhy04050@naver.com>
2023-05-21 20:34:52 +0900

    css module CB사 모듈 개발 및 연동 불필요 파일 삭제 nginxUrl 수정

--- docker/fintech-mysql.sh
+++ docker/fintech-mysql.sh
@@ -1,1 +1,9 @@
-docker run -d --name fintech-mysql -e MYSQL_ROOT_PASSWORD="fintech" -e MYSQL_USER="fintech" -e MYSQL_DATABASE="fintech" -e MYSQL_PASSWORD="fintech" -p 3306:3306 mysql:latest
+docker run -d \
+--name fintech-mysql \
+-e platform=linux/x86_64 \
+-e MYSQL_ROOT_PASSWORD="fintech" \
+-e MYSQL_USER="fintech" \
+-e MYSQL_PASSWORD="fintech" \
+-e MYSQL_DATABASE="fintech" \
+-p 3306:3306 \
+--network docker_fintech mysql:latest
