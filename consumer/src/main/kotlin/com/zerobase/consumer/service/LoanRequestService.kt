01f6b005fba4c371ef4b8f7eeb54ab65e5ebf2e3
JaeYooooon <dhy04050@naver.com>
2023-05-21 17:25:00 +0900

    consumer module application 포트 충돌 방지 CB 모듈 구현 예정

---
+++ consumer/src/main/kotlin/com/zerobase/consumer/service/LoanRequestService.kt
@@ -0,0 +1,22 @@
+package com.zerobase.consumer.service
+
+import com.zerobase.domain.domain.LoanReview
+import com.zerobase.domain.repository.LoanReviewRepository
+import org.springframework.stereotype.Service
+
+@Service
+class LoanRequestService(
+        private val loanReviewRepository: LoanReviewRepository
+) {
+
+    fun loanRequest() {
+        // TODO : CB Component 로 요청 보내기 -> 응답값을 DB에 저장하기
+    }
+
+    private fun loanRequestToCb() {
+        // TODO
+    }
+
+    private fun saveLoanReviewData(loanReview: LoanReview) = loanReviewRepository.save(loanReview)
+
+}
